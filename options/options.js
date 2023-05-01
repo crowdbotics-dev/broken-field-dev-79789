b94fbebd5ac429707648a6aff09493e069c34267
Crowdbotics <engineering@crowdbotics.com>
2023-05-01 06:21:59 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "broken-field-dev-79789",
+  url: "https://broken-field-dev-79789.botics.co",
+  api: "https://broken-field-dev-79789.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
